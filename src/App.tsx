ce28c0ec0a83de2460da73c094d8548b3102bdb8
Vanessa Campioni <vanessacrtsilva@gmail.com>
2023-09-12 21:14:01 -0300

    feat: add text area fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
-function App() {
+import { Github } from "lucide-react"
+import { Button } from "./components/ui/button"
+import { Separator } from "./components/ui/separator"
+import { Textarea } from "./components/ui/textarea"
+
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2"/>
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed" 
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed" 
+              placeholder="Resultado gerado pela IA..."
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável 
+            <code className="text-violet-400">{`{transcription}`}</code> 
+            no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+        <aside className=""></aside>
+      </main>
+    </div>
   )
 }
-
-export default App
